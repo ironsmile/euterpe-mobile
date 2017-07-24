c95e07cca37a90339aa561a749527247820df147
Doychin Atanasov <ironsmile@gmail.com>
2017-07-25 01:20:43 +0300

    Use redux and save state for navigation in AsyncStorage

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
-import { AppRegistry } from 'react-native';
+import React from 'react';
+import { AppRegistry, AppState, AsyncStorage } from 'react-native';
 import { TabNavigator } from 'react-navigation';
 import { HomeScreen } from './screens/home';
 import { BrowseScreen } from './screens/browse';
@@ -9,31 +10,111 @@ import { hs } from './styles/global';
 import TabBarBottom from './screens/common/TabBarBottom';
 import { TABBAR_HEIGHT } from './screens/common/footer';
 
+import { addNavigationHelpers } from 'react-navigation';
+import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
+import { connect, Provider } from 'react-redux';
+import { persistStore, autoRehydrate } from 'redux-persist'
+import { REHYDRATE } from 'redux-persist/constants'
+
+
+const navOptions = {
+    tabBarPosition: 'bottom',
+    animationEnabled: false,
+    tabBarComponent: TabBarBottom,
+    tabBarOptions: {
+        activeTintColor: 'white',
+        inactiveTintColor: '#bdbec2',
+        upperCaseLabel: false,
+        showIcon: true,
+        style: {
+            backgroundColor: '#222327',
+            height: TABBAR_HEIGHT,
+            position: 'absolute',
+            bottom: 0,
+            right: 0,
+            left: 0,
+        },
+    },
+};
+
 const HttpmsApp = TabNavigator({
     Home: { screen: HomeScreen, },
     Browse: { screen: BrowseScreen },
     Search: { screen: SearchScreen },
     Library: { screen: LibraryScreen },
     About: { screen: AboutScreen },
-}, {
-    tabBarPosition: 'bottom',
-    animationEnabled: false,
-    tabBarComponent: TabBarBottom,
-    initialRouteName: 'Search',
-    tabBarOptions: {
-      activeTintColor: 'white',
-      inactiveTintColor: '#bdbec2',
-      upperCaseLabel: false,
-      showIcon: true,
-      style: {
-        backgroundColor: '#222327',
-        height: TABBAR_HEIGHT,
-        position: 'absolute',
-        bottom: 0,
-        right: 0,
-        left: 0,
-      },
-    },
+}, navOptions);
+
+const homeParams = HttpmsApp.router.getActionForPathAndParams('Home');
+const initialState = HttpmsApp.router.getStateForAction(homeParams);
+
+const navReducer = (state = initialState, action) => {
+    const nextState = HttpmsApp.router.getStateForAction(action, state);
+
+    // Simply return the original `state` if `nextState` is null or undefined.
+    return nextState || state;
+};
+
+const appReducer = combineReducers({
+    nav: navReducer,
 });
 
-AppRegistry.registerComponent('httpms', () => HttpmsApp);
+const rehydratedReducer = (state = {}, action) => {
+    switch (action.type) {
+        case REHYDRATE:
+            var incoming = action.payload.myReducer
+            if (incoming) {
+                return {...state, ...incoming}
+            }
+        default:
+            return appReducer(state, action);
+    }
+}
+
+const mapStateToProps = (state) => ({
+    nav: state.nav
+});
+
+class App extends React.Component {
+  render() {
+    return (
+        <HttpmsApp
+            navigation={addNavigationHelpers({
+                dispatch: this.props.dispatch,
+                state: this.props.nav,
+            })}
+        />
+    );
+  }
+};
+
+const AppWithNavigationState = connect(mapStateToProps)(App);
+
+const store = createStore(
+    rehydratedReducer,
+    undefined,
+    compose(
+        autoRehydrate() 
+    )
+);
+
+persistStore(store, {storage: AsyncStorage});
+
+class Root extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            store: store
+        }
+    }
+
+    render() {
+        return (
+            <Provider store={this.state.store}>
+                <AppWithNavigationState />
+            </Provider>
+        );
+    }
+}
+
+AppRegistry.registerComponent('httpms', () => Root);
--- app/screens/common/footer.js
+++ app/screens/common/footer.js
@@ -37,7 +37,7 @@ export default class Footer extends Component {
 
 
     componentDidMount() {
-        setTimeout(() => this.measureView(), 0);
+        this.measureView();
     }
 
     measureView() {
