f27873a3aef537ba717aad66ee3da4d1eb0aa629
Doychin Atanasov <ironsmile@gmail.com>
2017-09-02 12:07:59 +0300

    Create a browse screen which does not handle any touches yet

--- app/screens/browse.js
+++ app/screens/browse.js
@@ -1,24 +1,153 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import { Screen } from '@screens/screen';
-import { Helpful } from '@components/helpful';
+import { SmallClickable } from '@components/small-clickable';
+import { PlatformIcon } from '@components/platform-icon';
+import { headerHeight } from '@screens/common/header';
 
 export class BrowseScreen extends React.Component {
+
+    getArtistsIcon() {
+        return (
+            <PlatformIcon
+                style={styles.sectionIcon}
+                platform="microphone"
+                color="#aeafb3"
+                size={30}
+            />
+        );
+    }
+
+    getAlbumsIcon() {
+        return (
+            <PlatformIcon
+                style={styles.sectionIcon}
+                platform="musical-notes"
+                color="#aeafb3"
+                size={30}
+            />
+        );
+    }
+
+    getLibraryIcon() {
+        return (
+            <PlatformIcon
+                style={styles.sectionIcon}
+                ios="ios-book"
+                md="ios-book"
+                color="#aeafb3"
+                size={30}
+            />
+        );
+    }
 
     render() {
         return (
             <Screen
-                title='BROWSE'
+                title="BROWSE"
                 navigation={this.props.navigation}
             >
-                <Helpful
-                    iconName="alert"
-                    title="Unsupported"
-                    firstLine="The HTTPMS server doesn't support browsing."
-                    secondLine="Upgrade it to the latest version."
-                />
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                >
+                    <View style={styles.headerAvoider} />
+
+                    <View style={styles.encouragmentContainer}>
+                        <Text style={styles.subduedText}>
+                            Not sure what exactly do you feel like listening to?
+                            I have an advice for you. Just browse around your
+                            music and in no time you will find something. Promise!
+                        </Text>
+                    </View>
+
+                    <SmallClickable
+                        onSelect={() => {}}
+                        mainText="Browse Artists"
+                        leftRectangle={this.getArtistsIcon()}
+                        rightIcon="ios-arrow-forward"
+                    />
+
+                    <SmallClickable
+                        onSelect={() => {}}
+                        mainText="Browse Albums"
+                        leftRectangle={this.getAlbumsIcon()}
+                        rightIcon="ios-arrow-forward"
+                    />
+
+                    <SmallClickable
+                        onSelect={() => {
+                            this.props.navigation.navigate('Library');
+                        }}
+                        mainText="Offline Library"
+                        leftRectangle={this.getLibraryIcon()}
+                        rightIcon="ios-arrow-forward"
+                    />
+
+                    <View style={styles.encouragmentContainer}>
+                        <Text style={styles.subduedText}>
+                            Don't feel like going through all of your music?
+                            No problem with that! Try searching:
+                        </Text>
+                    </View>
+
+                    <View style={[styles.encouragmentContainer, styles.noMargins]}>
+                        <TouchableOpacity
+                            onPress={() => {
+                                this.props.navigation.navigate('Search');
+                            }}
+                        >
+                            <View style={styles.searchButton}>
+                                <PlatformIcon platform="search" color="white" size={16} />
+                                <Text style={styles.searchText}>SEARCH</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </ScrollView>
             </Screen>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    headerAvoider: {
+        width: 100,
+        height: headerHeight,
+    },
+    sectionIcon: {
+        alignSelf: 'center',
+        paddingLeft: 10,
+    },
+    subduedText: {
+        'color': '#aeafb3',
+        textAlign: 'center',
+    },
+    encouragmentContainer: {
+        marginTop: 20,
+        marginBottom: 20,
+        marginLeft: 50,
+        marginRight: 50,
+        alignItems: 'center',
+    },
+    noMargins: {
+        marginTop: 0,
+        marginBottom: 0,
+    },
+    searchButton: {
+        backgroundColor: '#222327',
+        borderRadius: 10,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 10,
+        paddingRight: 20,
+        borderWidth: 2,
+        borderColor: '#3c3d41',
+        flexDirection: 'row',
+    },
+    searchText: {
+        paddingLeft: 10,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+});
