efef1b3f86ee55fd9a5d78237f2c561709f83aa1
Doychin Atanasov <ironsmile@gmail.com>
2021-04-20 21:40:38 +0300

    Remove unused images from the assets directory

--- app/assets/images.js
+++ app/assets/images.js
@@ -1,8 +1,4 @@
 const images = {
-  coverImage1: require('./images/1.jpg'),
-  coverImage2: require('./images/2.jpeg'),
-  coverImage3: require('./images/3.jpg'),
-
   unknownArtist: require('./images/unknownArtist.png'),
   unknownAlbum: require('./images/unknownAlbum.png'),
 };
